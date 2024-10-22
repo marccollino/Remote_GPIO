6bb1373b3a0c66d420996071a3633e71881ccb81
marccollino <marco.schaefer@htwg-konstanz.de>
2024-10-22 11:35:13 +0200

    IMPROVED: Pin edge detection and control with pigio

--- project-root/src/GPIO_module/GPIOs.py
+++ project-root/src/GPIO_module/GPIOs.py
@@ -1,9 +1,5 @@
-import RPi.GPIO as GPIO
 import time
-import threading
-import os
-import queue
-import psutil
+import pigpio # The pigpio library is written in the C programming language.The pigpio daemon offers a socket and pipe interface to the underlying C library. A C library and a Python module allow control of the GPIO via the pigpio daemon.
 
 # Set up the GPIO pins
 ULTRASONIC_TRIGGER_PIN = 23
@@ -18,53 +14,64 @@ startTime = 0
 stopTime = 0
 edgecount = 0
 
+pi = None
+cb = None
+
 # # Disable warnings regarding GPIO usage
 # GPIO.setwarnings(False)
 
-def edge_detected_callback(channel):
-    timeBuffer = time.perf_counter()
+def edge_detected_callback(gpio, level, tick):
     global startTime, stopTime, edgecount, duration 
+
     if edgecount == 0:
-        startTime = timeBuffer
+        startTime = tick # microseconds
         edgecount += 1
     elif edgecount == 1:
-        stopTime = timeBuffer
+        stopTime = tick
         duration = stopTime - startTime
+        edgecount = 0  # Reset edgecount for the next measurement
 
 def setup_gpio():
-    # Set up the GPIO pins
-    GPIO.setmode(GPIO.BCM)
+    global pi, cb
+    # Initialize Pigpio
+    pi = pigpio.pi()
+
+    if not pi.connected:
+        exit()
 
     # Set up the ultrasonic sensor pins
-    GPIO.setup(ULTRASONIC_TRIGGER_PIN, GPIO.OUT)
-    GPIO.setup(ULTRASONIC_ECHO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(ULTRASONIC_ECHO_PIN, GPIO.BOTH, callback=edge_detected_callback)
+    pi.set_mode(ULTRASONIC_TRIGGER_PIN, pigpio.OUTPUT)
+    pi.set_mode(ULTRASONIC_ECHO_PIN, pigpio.INPUT)
+    cb = pi.callback(ULTRASONIC_ECHO_PIN, pigpio.EITHER_EDGE, edge_detected_callback)
 
 def cleanup_gpio():
-    GPIO.cleanup()
+    global pi, cb
+    cb.cancel()
+    pi.stop()
 
 def getDistanceFromSonic():
     global duration
     global edgecount
     try:
         # Set trigger to False (Low)
-        GPIO.output(ULTRASONIC_TRIGGER_PIN, False)
-        time.sleep(0.1)
+        pi.write(ULTRASONIC_TRIGGER_PIN, 0)
+        time.sleep(0.01)
         returnPackage = {'duration': 9999, 'distance': 9999, 'error': None}
 
         # setze Trigger auf HIGH
-        GPIO.output(ULTRASONIC_TRIGGER_PIN, True)
+        pi.write(ULTRASONIC_TRIGGER_PIN, 1)
         # setze Trigger nach 0.01ms aus LOW
         time.sleep(0.00001)
-        GPIO.output(ULTRASONIC_TRIGGER_PIN, False)
+        pi.write(ULTRASONIC_TRIGGER_PIN, 0)
     
         # Wait for the echo to be received
         while duration == 0:
-            time.sleep(0.00001)  # Sleep for 1ms to prevent busy waiting
+            time.sleep(0.01)  # Sleep for 1ms to prevent busy waiting
 
-        #Round the duration to 6 decimal places
-        duration = round(duration, 6)
-        returnPackage['duration'] = duration
+        returnPackage['duration'] =  duration # microseconds
+
+        # Convert the duration to seconds
+        duration = duration / 1000000  # seconds
         edgecount = 0
     
         # mit der Schallgeschwindigkeit (343200 mm/s) multiplizieren
@@ -73,6 +80,10 @@ def getDistanceFromSonic():
         #Round the distance to 2 decimal places
         distance = round(distance, 2)
         returnPackage['distance'] = distance
+        duration = 0
+        distance = 0
+
+        print(returnPackage)
 
     except Exception as e:
         returnPackage['error'] = f"An error occurred: {e}"
